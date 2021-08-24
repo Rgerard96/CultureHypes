f32ffbff26997101465bbd4935068f7e0887ac09
Rgerard96 <rashidgerard@hotmail.com>
2021-08-24 16:22:07 +0200

    add menu icon

--- components/Header.js
+++ components/Header.js
@@ -6,12 +6,21 @@ export default function Header() {
   const [modal, setModal] = useState('');
   console.log(modal);
   return (
-    <div className='px-5 py-2.5 border-b'>
+    <div className='px-5 py-3 border-b'>
       <Modals modal={modal} setModal={setModal} />
       <div className='container mx-auto flex justify-between items-center'>
-        <Link href='/'>
-          <h1 className='font-bold sm:text-2xl cursor-pointer'>CultureHypes</h1>
-        </Link>
+        <div className='flex items-center'>
+          <img
+            src='/icons/menu.svg'
+            alt='Menu'
+            className='mr-5 h-4 sm:hidden'
+          />
+          <Link href='/'>
+            <h1 className='font-bold sm:text-2xl cursor-pointer'>
+              CultureHypes
+            </h1>
+          </Link>
+        </div>
         <div className='flex items-center'>
           {/* <img
             className='w-6 cursor-pointer'
@@ -20,11 +29,20 @@ export default function Header() {
           /> */}
           <Link href='/checkout'>
             <div className='pr-4 sm:pr-6 text-black text-opacity-60 hover:text-opacity-100 border-r flex items-center cursor-pointer'>
-              <img src="/icons/shopping-basket.svg" alt="Cart" className='mr-2 h-5 ' />
+              <img
+                src='/icons/shopping-basket.svg'
+                alt='Cart'
+                className='mr-2 h-4 '
+              />
               <p>0</p>
             </div>
           </Link>
-          <img src="/icons/user.svg" alt="Account" className='ml-4 h-5 cursor-pointer' onClick={() => setModal('login')}/>
+          <img
+            src='/icons/user.svg'
+            alt='Account'
+            className='ml-4 h-4 cursor-pointer'
+            onClick={() => setModal('login')}
+          />
           <p
             className='hidden sm:block ml-6 text-black sm:text-opacity-60 hover:text-opacity-100 cursor-pointer border rounded-lg py-1 px-2 sm:border-none sm:p-0 sm:rounded-none'
             onClick={() => setModal('login')}
