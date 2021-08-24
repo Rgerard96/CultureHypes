6e690014c1fa7eadd94b37b088a41e64e0e20a3d
Rgerard96 <rashidgerard@hotmail.com>
2021-08-24 16:38:30 +0200

    update icons

--- components/ModalCloseButton.js
+++ components/ModalCloseButton.js
@@ -6,8 +6,11 @@ export default function ModalCloseButton({ closeModal }) {
       className='absolute top-5 right-5 cursor-pointer h-5 group'
       onClick={() => closeModal()}
     >
-      <div className='bg-black bg-opacity-60 group-hover:bg-opacity-100 rounded-lg h-5 w-0.5 transform rotate-45'></div>
-      <div className='bg-black bg-opacity-60 group-hover:bg-opacity-100 rounded-lg h-5 w-0.5 transform rotate-135 -translate-y-5'></div>
+      <img
+            src='/icons/close.svg'
+            alt='Menu'
+            className='h-4'
+          />
     </div>
   );
 }
