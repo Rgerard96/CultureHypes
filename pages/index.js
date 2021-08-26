5eabeeac3db14adeb158eabd9dd18a7f7cc582d7
Rgerard96 <rashidgerard@hotmail.com>
2021-08-26 20:09:38 +0200

    remove hover state

--- pages/index.js
+++ pages/index.js
@@ -19,39 +19,39 @@ export default function Home() {
         <div className=' bg-primary h-36 sm:h-52 rounded-lg mb-10'></div>
         <div className='grid gap-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5'>
           <div
-            className=' h-72 rounded-lg border border-primary cursor-pointer hover:bg-primary'
+            className=' h-72 rounded-lg border border-primary cursor-pointer'
             onClick={() => router.push('/food')}
           ></div>
           <div
-            className=' h-72 rounded-lg border border-primary cursor-pointer hover:bg-primary'
+            className=' h-72 rounded-lg border border-primary cursor-pointer'
             onClick={() => router.push('/food')}
           ></div>
           <div
-            className=' h-72 rounded-lg border border-primary cursor-pointer hover:bg-primary'
+            className=' h-72 rounded-lg border border-primary cursor-pointer'
             onClick={() => router.push('/food')}
           ></div>
           <div
-            className=' h-72 rounded-lg border border-primary cursor-pointer hover:bg-primary'
+            className=' h-72 rounded-lg border border-primary cursor-pointer'
             onClick={() => router.push('/food')}
           ></div>
           <div
-            className=' h-72 rounded-lg border border-primary cursor-pointer hover:bg-primary'
+            className=' h-72 rounded-lg border border-primary cursor-pointer'
             onClick={() => router.push('/food')}
           ></div>
           <div
-            className=' h-72 rounded-lg border border-primary cursor-pointer hover:bg-primary'
+            className=' h-72 rounded-lg border border-primary cursor-pointer'
             onClick={() => router.push('/food')}
           ></div>
           <div
-            className=' h-72 rounded-lg border border-primary cursor-pointer hover:bg-primary'
+            className=' h-72 rounded-lg border border-primary cursor-pointer'
             onClick={() => router.push('/food')}
           ></div>
           <div
-            className=' h-72 rounded-lg border border-primary cursor-pointer hover:bg-primary'
+            className=' h-72 rounded-lg border border-primary cursor-pointer'
             onClick={() => router.push('/food')}
           ></div>
           <div
-            className=' h-72 rounded-lg border border-primary cursor-pointer hover:bg-primary'
+            className=' h-72 rounded-lg border border-primary cursor-pointer'
             onClick={() => router.push('/food')}
           ></div>
         </div>
