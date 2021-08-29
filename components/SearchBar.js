d339d34648463954e8e6a5bded66bfd7b595d800
Rgerard96 <rashidgerard@hotmail.com>
2021-08-29 14:29:53 +0200

    adjust icons

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -5,7 +5,7 @@ export default function SearchBar() {
     <div className='px-5 py-3 border-b sticky top-0 bg-white'>
       <div className='container flex items-center justify-end mx-auto'>
         <div className='group focus-within:border-primary flex items-center border rounded-lg px-3 py-2 w-full sm:w-96'>
-          <img src='/icons/search.svg' alt='Search' className='h-5 mr-3' />
+          <img src='/icons/search.svg' alt='Search' className='h-4 mr-3' />
           <input
             type='search'
             name='search'
@@ -14,7 +14,7 @@ export default function SearchBar() {
             className='border-none w-full focus:outline-none focus:ring-0 p-0'
           />
         </div>
-        <img src='/icons/filter.svg' alt='Filter' className='ml-5 h-5 cursor-pointer' />
+        <img src='/icons/filter.svg' alt='Filter' className='ml-5 h-4 cursor-pointer' />
       </div>
     </div>
   );
